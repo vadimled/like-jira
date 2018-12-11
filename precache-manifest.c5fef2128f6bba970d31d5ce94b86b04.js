1a5a37913044b9589ee19a0371c5b9704ae9e553
Vadim Malckin <vadimproveril@gmail.com>
2018-12-12 00:44:05 +0200

    Updates

---
+++ precache-manifest.c5fef2128f6bba970d31d5ce94b86b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db628cae56a157622c47",
+    "url": "/like-jira/static/js/runtime~main.db628cae.js"
+  },
+  {
+    "revision": "8edfa90ada4c5b919ee2",
+    "url": "/like-jira/static/js/main.8edfa90a.chunk.js"
+  },
+  {
+    "revision": "5111f1608de2ff957c8f",
+    "url": "/like-jira/static/js/1.5111f160.chunk.js"
+  },
+  {
+    "revision": "8edfa90ada4c5b919ee2",
+    "url": "/like-jira/static/css/main.00a3eb9f.chunk.css"
+  },
+  {
+    "revision": "5111f1608de2ff957c8f",
+    "url": "/like-jira/static/css/1.97df014e.chunk.css"
+  },
+  {
+    "revision": "bdd5647bf4edab80a79ea81518e3ea5c",
+    "url": "/like-jira/index.html"
+  }
+];
